43eaf49f580a0965365af53acabe706a8c2aca09
Kishlay Kishan <kishan.kishlay20@gmail.com>
2022-04-04 22:15:18 +0530

    Added:Edit Existing Contact To Address Book.

--- src/com/bridgelabz/addressbook/Addressbook.java
+++ src/com/bridgelabz/addressbook/Addressbook.java
@@ -1,107 +1,100 @@
 package com.bridgelabz.addressbook;
 
-class Contact {
-
-	private String firstName;
-	private String lastName;
-	private String address;
-	private String city;
-	private int zipcode;
-	private String state;
-	private String phoneNo;
-	private String email;
-
-	public Contact(String firstName, String lastName, String address, String city, int zipcode, String state,
-			String phoneNo, String email) {
-		setFirstName(firstName);
-		setLastName(lastName);
-		setAddress(address);
-		setCity(city);
-		setState(state);
-		setZipcode(zipcode);
-		setPhoneNo(phoneNo);
-		setEmail(email);
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public int getZipcode() {
-		return zipcode;
-	}
-
-	public void setZipcode(int zipcode) {
-		this.zipcode = zipcode;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	public String getPhoneNo() {
-		return phoneNo;
-	}
-
-	public void setPhoneNo(String phoneNo) {
-		this.phoneNo = phoneNo;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	@Override
-	public String toString() {
-		return "Contact [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city=" + city
-				+ ", zipcode=" + zipcode + ", state=" + state + ", phoneNo=" + phoneNo + ", email=" + email + "]";
-	}
-
-}
+import java.util.ArrayList;
+import java.util.ListIterator;
+import java.util.Scanner;
 
 public class Addressbook {
-
 	public static void main(String[] args) {
-		System.out.println("Welcome to Address Book Programs");
-		Contact contact = new Contact(" Kishlay", "Kishan", "Aghoria Bazar", "Muzaffarpur", 842002, "Bihar",
-				"7979034653", "kishan.kishlay20@gmail.com");
-		System.out.println(contact);
+		System.out.println("Welcome to the address book system");
+		ArrayList<Contact> array = new ArrayList<Contact>();
+		try (Scanner sc = new Scanner(System.in)) {
+			try (Scanner sc1 = new Scanner(System.in)) {
+				int choice;
+				do {
+					System.out.println("1.INSERT" + "\n2.DISPLAY" + "\n3.EDIT");
+					System.out.println("Enter your 1st choice");
+					choice = sc.nextInt();
 
+					switch (choice) {
+					case 1:
+						System.out.print("Enter First Name: ");
+						String firstName1 = sc1.nextLine();
+						System.out.print("Enter last Name: ");
+						String lastName = sc1.nextLine();
+						System.out.print("Enter your address: ");
+						String address = sc1.nextLine();
+						System.out.print("Enter your city: ");
+						String city = sc1.nextLine();
+						System.out.print("Enter your state: ");
+						String state = sc1.nextLine();
+						System.out.print("Enter zip code : ");
+						int zipcode = sc.nextInt();
+						System.out.print("Enter phone number: ");
+						String phoneNo = sc1.nextLine();
+						System.out.print("Enter your email: ");
+						String email = sc1.nextLine();
+						Contact contact = new Contact(firstName1, lastName, address, city, state, zipcode, phoneNo,
+								email);
+						array.add(contact);
+						break;
+					case 2:
+						System.out.println(array);
+						break;
+					case 3:
+						boolean found = false;
+						System.out.println("Enter first name of the person for update ");
+						firstName1 = sc1.nextLine();
+						System.out.println("------------------------------------");
+
+						ListIterator<Contact> listIterator = array.listIterator();
+						/*
+						 * It is a java iterator which is used to traverse all types of lists including
+						 * ArrayList, Vector, LinkedList, Stack
+						 */
+
+						while (listIterator.hasNext()) {
+							contact = listIterator.next();
+
+							if (contact.getFirstName().equals(firstName1)) { // entering the condition if fname matches
+
+								System.out.print("Enter new first Name: ");
+								firstName1 = sc1.nextLine();
+								System.out.print("Enter new last Name: ");
+								lastName = sc1.nextLine();
+								System.out.print("Enter your new address: ");
+								address = sc1.nextLine();
+								System.out.print("Enter your new city: ");
+								city = sc1.nextLine();
+								System.out.print("Enter your state: ");
+								state = sc1.nextLine();
+								System.out.print("Enter zip code : ");
+								zipcode = sc.nextInt();
+								System.out.print("Enter phone number: ");
+								phoneNo = sc1.nextLine();
+								System.out.print("Enter your eMail ID: ");
+								email = sc1.nextLine();
+								listIterator.set(new Contact(firstName1, lastName, address, city, state, zipcode,
+										phoneNo, email));
+								found = true;
+							}
+						}
+						System.out.println("_____________________________________");
+
+						if (!found) {
+							System.out.println("Record not found");// o/p for mismatch first name
+						} else {
+							System.out.println("Record is updated successfully");
+						}
+						System.out.println("_______________________________________");
+						break;
+					default:
+						System.out.println("default");
+
+					}
+				} while (choice != 0);
+				System.out.println("Invalid Choice!!");
+			}
+		}
 	}
 }
