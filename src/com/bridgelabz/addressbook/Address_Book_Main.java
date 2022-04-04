1393e287452306e9cbdb817180a0a9e2f1a6390e
Kishlay Kishan <kishan.kishlay20@gmail.com>
2022-04-04 22:38:22 +0530

    Added:Add Multiple Address Book.

--- src/com/bridgelabz/addressbook/Address_Book_Main.java
+++ src/com/bridgelabz/addressbook/Address_Book_Main.java
@@ -1,8 +1,46 @@
 package com.bridgelabz.addressbook;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Address_Book_Main {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Address Book Programs u");
+	static Scanner sc = new Scanner(System.in);
+	HashMap<String, Addressbook> addressbook = new HashMap<>();
+
+	/*
+	 * Add Multiple Address book
+	 * 
+	 */
+
+	public void addMultipleAddressBook() {
+		while (true) {
+
+			System.out.println("Enter selection \n1)To Access Address Book\n2)exit");
+			int selection = sc.nextInt();
+			switch (selection) {
+			case 1:
+				String name = sc.next();
+				if (addressbook.containsKey(name)) {
+					System.out.println("Entered Addressbook name already present");
+				} else {
+					Addressbook address = new Addressbook();
+					System.out.println("Welcome to addressbook" + name);
+					addressbook.put(name, address);
+				}
+				break;
+
+			case 2:
+				System.out.println("exiting");
+				System.exit(0);
+			}
+
+		}
 
 	}
+
+	public static void main(String[] args) {
+		Address_Book_Main main = new Address_Book_Main();
+		main.addMultipleAddressBook();
+	}
+
 }
