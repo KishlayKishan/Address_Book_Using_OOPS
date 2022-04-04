98f5de4c7bc8e3b068ac467951e7b7889b977675
Kishlay Kishan <kishan.kishlay20@gmail.com>
2022-04-04 13:20:37 +0530

    Added:Welcome to Address book system problem using OOPS Concept

---
+++ src/com/bridgelabz/addressbook/Address_Book_Main.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.addressbook;
+
+public class Address_Book_Main {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program");
+
+	}
+
+}
